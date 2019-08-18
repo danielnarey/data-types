22a1ac560fc222d53bf4a80e21f54f85d2473c6e
Daniel Narey <danielnarey@users.noreply.github.com>
2019-08-17 21:58:51 -0400

    sync

--- src/convert-sync.js
+++ src/convert-sync.js
@@ -43,14 +43,14 @@ const toNumber = (valueMap = null, typeMap = new Map([['Undefined', NaN], ['Null
 
 // EXPOSED: MODULE, PACKAGE
 // [function], [Map] => * => boolean
-const toBoolean = (test = x => Boolean(x), typeMap = new Map([['Undefined', false], ['Null', false]])) => (value) => {
+const toBoolean = (test = (x => Boolean(x)), typeMap = new Map([['Undefined', false], ['Null', false]])) => (value) => {
   const valueType = whatType(value);
 
   if (typeMap.has(valueType)) {
     return typeMap.get(valueType);
   }
 
-  return Function.prototype.call(test, value);
+  return test(value);
 };
 
 
